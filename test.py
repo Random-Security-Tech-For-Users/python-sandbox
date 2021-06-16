7623c3647ebaa0a7c36e2ef7caaaab058bb87a5e
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-16 08:45:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import importlib
 def importer(name, *args):
     M = importlib.__import__(name, *args)
-    if name == “os”:
+    if name == 'os':
         del M.system
     return M
 __builtins__.__dict__['__import__'] = importer
