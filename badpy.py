bd601149ef21bce2c15e23e3a204ede9c292352b
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-16 08:57:28 -0400

    Update badpy.py

--- badpy.py
+++ badpy.py
@@ -1,7 +1,7 @@
-import test
+#import test
 import os
 try:
-  os.system("echo 1")
+  os.system("echo \"do bad things\"")
 except Exception as err:
   print("os.system errored with: {}".format(err))
 else:
